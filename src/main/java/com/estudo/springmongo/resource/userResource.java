06eda8674c3d099cb86c04439102d7c78acaa529
Geovanni dos Santos Correia <geovanni-dsc@hotmail.com>
2022-11-02 23:03:37 -0300

    Atualizando Usuario com PUT

--- src/main/java/com/estudo/springmongo/resource/userResource.java
+++ src/main/java/com/estudo/springmongo/resource/userResource.java
@@ -58,5 +58,16 @@ public class userResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<UserDTO> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+	  
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 }
